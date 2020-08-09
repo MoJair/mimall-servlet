a054581f8e8292d6ad5262d44a87cf61cab95845
MoJair <1639269322@qq.com>
2020-08-10 00:30:05 +0800

    登录改动

--- src/com/mi/dao/impl/MemberInfoDaoImpl.java
+++ src/com/mi/dao/impl/MemberInfoDaoImpl.java
@@ -9,7 +9,7 @@ public class MemberInfoDaoImpl implements MemberInfoDao{
 	@Override
 	public MemberInfo login(String account, String pwd) {
 		DBHelper db = new DBHelper();
-		String sql = "select uid, uname, tel, email from memberinfo where (uname=? or tel=? or email=?) and pwd=md5(?)";
+		String sql = "select uid, uname, tel, email from userInfo where (uname=? or tel=? or email=?) and pwd=?";
 		return db.find(MemberInfo.class, sql, account, account, account, pwd);
 	}
 	
